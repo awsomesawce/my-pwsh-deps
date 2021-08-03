c0872ba082339b34c8a6c3144680d08109c6db1e
Carl C <awsomesawce@outlook.com>
2021-08-03 04:53:38 -0400

    update psenv

--- envs/.psenv.ps1
+++ envs/.psenv.ps1
@@ -5,6 +5,20 @@
 
 # Functions relating to npm dev workflow
 
+function findmoduledocs {
+
+    <#
+    .Description
+    If a docs folder exists in a node module, list
+    the contents of it.
+#>
+    switch (Test-Path ".\node_modules\$args\docs") {
+	$true { ls ".\node_modules\$args/docs" }
+	$false { "It is not there" }
+    }
+
+}
+
 function Page-Readme {
     <#
     .Syntax
