b99883ab57f567b89465abfeb4241b404236fa18
Carl C <awsomesawce@outlook.com>
2021-08-19 22:44:02 -0400

    Update writecolors

--- writecolors/WriteColors.psm1
+++ writecolors/WriteColors.psm1
@@ -1,30 +1,26 @@
-# OriginalLocation: https://github.com/awsomesawce/scripts-pwsh/ScriptsAndFunctions/dependencies/WriteColors.ps1
-
 # WriteColors.ps1
 # Contains functions that wrap around Write-Host to get colorized output
 #
 # :alternatives: Using nodejs's ansi-colors or chalk to colorize output.
 # :NOTE: Write-Host is much easier to use as a dependency for pwsh scripts than nodejs chalk would be
+<#
+.Description
+WriteColors.psm1 - wrappers around Write-Host
+.NOTES
+This was predominantly a test on how to make easily importable modules for use
+in homemade scripts and functions, including PROFILE
+.Author Carl C.
+#>
 
-function whc {
-    <#
-    .Description
-    Base wrapper function around Write-Host.
-    .EXAMPLE
-    whc -color Yellow -msg "hello there"
-    #>
-    param([string]$color, [string]$msg)
-    if ($color) {
-	return Write-Host -ForegroundColor $color "$msg"
-    }
-}
+
+Set-Alias whc -Value Write-Host -Description "Shortening of Write-Host's command"
 
 function echoCyan {
     <#
     .Description
     Wrapper around write-host to output text in cyan
     #>
-    return whc -color Cyan -msg "$args"
+    return whc -Foregroundcolor Cyan -msg "$args"
 }
 
 function echoYellow {
@@ -32,7 +28,7 @@ function echoYellow {
     .Description
     Wrapper around write-host to output text in Yellow
     #>
-    return whc -color Yellow -msg "$args"
+    return whc -foregroundcolor Yellow -msg "$args"
 }
 
 function echoRed {
