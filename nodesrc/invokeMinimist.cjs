5d02d6c0f53f233d5b69311de203ce221a6412b4
Carl C <awsomesawce@outlook.com>
2021-08-20 23:56:46 -0400

    Add sublime workspace and proj

--- nodesrc/invokeMinimist.cjs
+++ nodesrc/invokeMinimist.cjs
@@ -1,12 +1,38 @@
 #!/usr/bin/env node
 // Minimist example
 
-const argv = require('minimist')(process.argv.slice(2))
+const fs = require('fs-extra')
+const path = require('path')
+const argv = require('minimist')(process.argv)
 
-if (argv[1] == "y") {
-	console.log("you picked y");
-} elseif (argv[1] == "n") {
-	console.log("you picked n)
+let myArgs = require('minimist')(process.argv.slice(2))
+
+/*
+let args = argv._
+
+let firstArg = args[0]
+
+if (firstArg == "--help") {
+	console.log()
 }
+
+if (args[0] == "y") {
+	console.log("you picked y");
+} 
+if (args[0] == "n") {
+	console.log("you picked n");
+} else {
+	console.log("else");
+}
+*/
+
+function displayArgs() {
+	let firstArg = argv._[0]
+	if (firstArg.match("node")) {
+		console.log(`firstArg matches node: ${firstArg}`)
+	}
+}
+
+displayArgs()
 
 console.log("done")
